75f328329871fc9c22324db8ea4501702936a43c
giadz2005 <quocgia2005@gmail.com>
2026-03-07 08:54:54 +0700

    feat: add middleware validation

---
+++ backend/src/middleware/validate.middleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import { ZodObject } from 'zod';
+import { catchAsync } from '@/utils/catchAsync';
+import { ParsedQs } from 'qs';
+import { ParamsDictionary } from 'express-serve-static-core';
+
+export const validate = (schema: ZodObject) => {
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const parsed = await schema.parseAsync({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+    });
+
+    req.body = parsed.body;
+    req.query = parsed.query as ParsedQs;
+    req.params = parsed.params as ParamsDictionary;
+
+    next();
+  });
+};
