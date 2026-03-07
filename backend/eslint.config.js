4e66368ab017cd58d70e951a5a92d8f87b7e2ea5
giadz2005 <quocgia2005@gmail.com>
2026-03-07 14:12:29 +0700

    fix: update eslint configuration

---
+++ backend/eslint.config.js
@@ -0,0 +1,26 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    // Cấu hình các quy tắc riêng
+    rules: {
+      'no-console': 'warn', // Cảnh báo khi dùng console.log
+      'prefer-const': 'error', // Bắt buộc dùng const nếu biến không thay đổi
+      '@typescript-eslint/no-explicit-any': 'warn', // Hạn chế dùng kiểu "any"
+      quotes: [2, 'single'],
+      'no-undef': 'off',
+    },
+  },
+  {
+    // Chỉ định các file cần kiểm tra
+    files: ['src/**/*.ts'],
+    // Bỏ qua các thư mục không cần thiết
+    ignores: ['node_modules/', 'dist/'],
+  },
+  eslintConfigPrettier,
+);
