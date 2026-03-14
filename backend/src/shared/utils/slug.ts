75e187010dcb2eaf8d2d5e54f6f7e440eda07fb9
giadz2005 <quocgia2005@gmail.com>
2026-03-14 11:28:44 +0700

    feat: add slug utility

---
+++ backend/src/shared/utils/slug.ts
@@ -0,0 +1,17 @@
+import { randomUUID } from 'crypto';
+import slugify from 'slugify';
+
+export const slug = {
+  generate: (text: string) => {
+    const base = slugify(text, {
+      lower: true,
+      strict: true,
+      locale: 'vi',
+      trim: true,
+    });
+
+    const suffix = randomUUID().slice(0, 6);
+
+    return `${base}-${suffix}`;
+  },
+};
