8f0522875a233f7964e341f5e9eb28d2e7e52ef7
giadz2005 <quocgia2005@gmail.com>
2026-03-17 11:16:54 +0700

    feat: add success message for list and detail product APIs

--- backend/src/modules/products/product.controller.ts
+++ backend/src/modules/products/product.controller.ts
@@ -4,18 +4,27 @@ import { productService } from './product.service';
 import { successResponse } from '@/shared/utils/response';
 import { ImageType } from '@/shared/types/image.type';
 import { imageService } from '@/shared/services/image.service';
+import { productSchema } from './product.schema';
+import { MESSAGE } from '@/shared/constants/message.constants';
 
 export const productController = {
   getAll: catchAsync(async (req: Request, res: Response) => {
-    const products = await productService.getAll();
+    const { query } = productSchema.productQuery.parse(req);
+    const result = await productService.getAll(query);
 
-    successResponse(res, 200, 'Lấy danh sách sản phẩm thành công', products);
+    successResponse(
+      res,
+      200,
+      MESSAGE.PRODUCT.GET_LIST_SUCCESS,
+      result.data,
+      result.meta,
+    );
   }),
 
   getBySlug: catchAsync(async (req: Request, res: Response) => {
     const product = await productService.getBySlug(req.params.slug as string);
 
-    successResponse(res, 200, 'Lấy sản phẩm thành công', product);
+    successResponse(res, 200, MESSAGE.PRODUCT.GET_DETAIL_SUCCESS, product);
   }),
 
   create: catchAsync(async (req: Request, res: Response) => {
@@ -29,7 +38,7 @@ export const productController = {
 
     const product = await productService.create(req.body, images);
 
-    successResponse(res, 201, 'Tạo sản phẩm thành công', product);
+    successResponse(res, 201, MESSAGE.PRODUCT.CREATED_SUCCESS, product);
   }),
 
   update: catchAsync(async (req: Request, res: Response) => {
@@ -47,12 +56,12 @@ export const productController = {
       images,
     );
 
-    successResponse(res, 200, 'Cập nhật sản phẩm thành công', product);
+    successResponse(res, 200, MESSAGE.PRODUCT.UPDATED_SUCCESS, product);
   }),
 
   delete: catchAsync(async (req: Request, res: Response) => {
     await productService.delete(req.params.id as string);
 
-    successResponse(res, 200, 'Xóa sản phẩm thành công');
+    successResponse(res, 200, MESSAGE.PRODUCT.DELETED_SUCCESS);
   }),
 };
