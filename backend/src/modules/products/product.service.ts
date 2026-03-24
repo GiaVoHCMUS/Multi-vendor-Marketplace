2b7c73ee15597209c938a4f3e97684464f30e7aa
giadz2005 <quocgia2005@gmail.com>
2026-03-24 11:25:04 +0700

    fix: add stock, status, description in response of getAll

--- backend/src/modules/products/product.service.ts
+++ backend/src/modules/products/product.service.ts
@@ -19,192 +19,6 @@ export const productService = {
     // Tăng version -> Mọi query list sau đó sẽ bị cache miss và lấy data mới từ DB
     await cacheService.invalidateTracker(CACHE_KEYS.PRODUCT.TRACKER_LIST);
   },
-  // async getAll(queryInput: ProductQueryInput) {
-  //   const {
-  //     limit,
-  //     cursor,
-  //     categorySlug,
-  //     shopSlug,
-  //     search,
-  //     minPrice,
-  //     maxPrice,
-  //   } = queryInput;
-
-  //   const [category, shop] = await Promise.all([
-  //     categorySlug
-  //       ? cacheService.getOrSet(
-  //           CACHE_KEYS.CATEGORY.ID_BY_SLUG(categorySlug),
-  //           () =>
-  //             prisma.category.findUnique({
-  //               where: { slug: categorySlug },
-  //               select: { id: true },
-  //             }),
-  //           CACHE_TTL.LONG,
-  //         )
-  //       : null,
-
-  //     shopSlug
-  //       ? cacheService.getOrSet(
-  //           CACHE_KEYS.SHOP.ID_BY_SLUG(shopSlug),
-  //           () =>
-  //             prisma.shop.findUnique({
-  //               where: { slug: shopSlug },
-  //               select: { id: true },
-  //             }),
-  //           CACHE_TTL.LONG,
-  //         )
-  //       : null,
-  //   ]);
-
-  //   if ((shopSlug && !shop) || (categorySlug && !category)) {
-  //     return {
-  //       data: [],
-  //       meta: {
-  //         limit,
-  //         nextCursor: null,
-  //         filters: queryInput,
-  //       },
-  //     };
-  //   }
-
-  //   // Lấy Versioning cho List Product
-  //   const version = await cacheService.getTracker(
-  //     CACHE_KEYS.PRODUCT.TRACKER_LIST,
-  //   );
-
-  //   // Hàm Helper để sort object keys
-  //   const sortObject = (obj: any) =>
-  //     Object.keys(obj)
-  //       .sort()
-  //       .reduce((res: any, key) => {
-  //         res[key] = obj[key];
-  //         return res;
-  //       }, {});
-
-  //   // Tạo unique key dựa trên các tham số filter và IDs
-  //   const cacheKey = CACHE_KEYS.PRODUCT.LIST(
-  //     version,
-  //     JSON.stringify(
-  //       sortObject({
-  //         ...queryInput,
-  //         categoryId: category?.id,
-  //         shopId: shop?.id,
-  //       }),
-  //     ),
-  //   );
-
-  //   return cacheService.getOrSet(
-  //     cacheKey,
-  //     async () => {
-  //       const where: any = {
-  //         deletedAt: null,
-  //         status: ProductStatus.PUBLISHED,
-  //       };
-
-  //       // Search bằng contains của Prisma
-  //       if (search) {
-  //         where.name = {
-  //           contains: search,
-  //           mode: 'insensitive',
-  //         };
-  //       }
-
-  //       // price
-  //       if (minPrice || maxPrice) {
-  //         where.price = {
-  //           gte: minPrice,
-  //           lte: maxPrice,
-  //         };
-  //       }
-
-  //       // category
-  //       if (category) {
-  //         where.categoryId = category.id;
-  //       }
-
-  //       // shop
-  //       if (shop) {
-  //         where.shopId = shop.id;
-  //       }
-
-  //       // Query
-  //       const products = await prisma.product.findMany({
-  //         where,
-  //         orderBy: {
-  //           id: 'desc', // cursor-safe
-  //         },
-  //         take: limit + 1,
-
-  //         cursor: cursor ? { id: cursor } : undefined,
-  //         skip: cursor ? 1 : 0,
-
-  //         select: {
-  //           id: true,
-  //           name: true,
-  //           slug: true,
-  //           price: true,
-  //           averageRating: true,
-
-  //           images: {
-  //             take: 1,
-  //             orderBy: { order: 'asc' },
-  //             select: { url: true },
-  //           },
-
-  //           shop: {
-  //             select: {
-  //               id: true,
-  //               name: true,
-  //               slug: true,
-  //             },
-  //           },
-
-  //           category: {
-  //             select: {
-  //               id: true,
-  //               name: true,
-  //               slug: true,
-  //             },
-  //           },
-  //         },
-  //       });
-
-  //       // Pagination logic
-  //       const hasNext = products.length > limit;
-  //       const items = hasNext ? products.slice(0, -1) : products;
-
-  //       // Format data
-  //       const data = items.map((p) => ({
-  //         id: p.id,
-  //         name: p.name,
-  //         slug: p.slug,
-  //         price: p.price,
-  //         averageRating: p.averageRating,
-  //         image: p.images[0]?.url || null,
-  //         shop: p.shop,
-  //         category: p.category,
-  //       }));
-
-  //       // Meta
-  //       return {
-  //         data,
-  //         meta: {
-  //           limit,
-  //           nextCursor: hasNext ? items[items.length - 1].id : null,
-
-  //           filters: {
-  //             search: search ?? null,
-  //             minPrice: minPrice ?? null,
-  //             maxPrice: maxPrice ?? null,
-  //             categorySlug: categorySlug ?? null,
-  //             shopSlug: shopSlug ?? null,
-  //           },
-  //         },
-  //       };
-  //     },
-  //     CACHE_TTL.SHORT,
-  //   );
-  // },
 
   async getAll(queryInput: ProductQueryInput) {
     const {
@@ -326,6 +140,9 @@ export const productService = {
             slug: true,
             price: true,
             averageRating: true,
+            description: true,
+            stock: true,
+            status: true,
 
             images: {
               take: 1,
@@ -381,6 +198,9 @@ export const productService = {
           price: p.price,
           averageRating: p.averageRating,
           image: p.images[0]?.url || null,
+          description: p.description,
+          stock: p.stock,
+          status: p.status,
           shop: p.shop,
           category: p.category,
         }));
